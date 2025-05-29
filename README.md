7d202773e943c5e2bdf65bfd49f3b7c56f867653
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2025-05-29 19:36:17 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# consumindo_api_com_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/consumindo_api_com_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.consumindo_api_com_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
