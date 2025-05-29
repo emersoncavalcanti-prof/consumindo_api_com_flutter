6eeaff7e03ff6eab37dda232c78e52d2f2fff1de
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2025-05-29 20:08:43 -0300

    add interface HttpClient e DioClient

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,10 +1,24 @@
+import 'package:consumindo_api_com_flutter/http/data/http_client.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    final dio = Dio();
+
+    return MultiProvider(
+      providers: [
+        Provider<Dio>.value(value: dio),
+        ProxyProvider<Dio, DioClient>(
+          update: (context, dio, dioClient) {
+            return DioClient(dio);
+          },
+        ),
+      ],
+    );
   }
 }
