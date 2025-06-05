1e6a03c69eec4f1044f5b673c6ff5f261b294ab4
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2025-06-05 20:21:24 -0300

    criando animacao do logar

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:consumindo_api_com_flutter/data/http/http_client.dart';
 import 'package:consumindo_api_com_flutter/data/repositories/user_repository.dart';
 import 'package:consumindo_api_com_flutter/pages/login/store/user_store.dart';
+import 'package:consumindo_api_com_flutter/widget/custom_edit.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -32,10 +33,72 @@ class _LoginPageState extends State<LoginPage> {
       ),
       body: Padding(
         padding: EdgeInsets.only(top: 30, left: 20, right: 20),
-        child: Form(key: _formkey, child: ListView(children: [
-            
-          ],
-        )),
+        child: Form(
+          key: _formkey,
+          child: ListView(
+            children: [
+              CustomEdit(
+                label: 'Digite seu email',
+                icone: Icon(Icons.email),
+                isObscure: false,
+                controller: controllerEmail,
+                validator: (email) {
+                  if (email == null || email.isEmpty) {
+                    return 'Digite seu email';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              CustomEdit(
+                label: 'Digite sua senha',
+                icone: Icon(Icons.lock),
+                isObscure: true,
+                controller: controllerPassword,
+                validator: (password) {
+                  if (password == null || password.isEmpty) {
+                    return 'Digite sua senha';
+                  }
+                  if (password.length < 4) {
+                    return 'Sua senha deve ter pelo menos 4 caracteres';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    clicou = !clicou;
+                  });
+                },
+                child: Center(
+                  child: AnimatedContainer(
+                    duration: Duration(milliseconds: 400),
+                    width: clicou == true ? 40 : width,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    child: Center(
+                      child:
+                          clicou == true
+                              ? CircularProgressIndicator(color: Colors.white)
+                              : Text(
+                                'Logar',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
