47403cba9d9c68357823890d47119e2135ea68ce
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2025-06-05 19:58:35 -0300

    criando widget custom_edit

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,4 +1,8 @@
+import 'package:consumindo_api_com_flutter/data/http/http_client.dart';
+import 'package:consumindo_api_com_flutter/data/repositories/user_repository.dart';
+import 'package:consumindo_api_com_flutter/pages/login/store/user_store.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,8 +12,31 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formkey = GlobalKey<FormState>();
+  final controllerEmail = TextEditingController();
+  final controllerPassword = TextEditingController();
+  bool clicou = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final dioClient = Provider.of<DioClient>(context);
+
+    UserStore store = UserStore(repository: UserRepository(client: dioClient));
+
+    final width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 30, left: 20, right: 20),
+        child: Form(key: _formkey, child: ListView(children: [
+            
+          ],
+        )),
+      ),
+    );
   }
 }
---
+++ lib/widget/custom_edit.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomEdit extends StatelessWidget {
+  final String label;
+  final Icon icone;
+  final bool isObscure;
+  final String? Function(String?)? validator;
+  final TextEditingController? controller;
+
+  const CustomEdit({
+    super.key,
+    required this.label,
+    required this.icone,
+    required this.isObscure,
+    this.validator,
+    this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      obscureText: isObscure,
+      controller: controller,
+      validator: validator,
+      decoration: InputDecoration(
+        prefixIcon: icone,
+        labelText: label,
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
+}
